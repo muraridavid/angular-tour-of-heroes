973641a91ed07d0d8875f09076d9859f792da773
muraridavid <david.murari@gmail.com>
2022-01-02 13:37:02 +0100

    20220102_1336

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
-})
+  styleUrls: ['./heroes.component.css'],
+  })
 export class HeroesComponent implements OnInit {
-
   constructor() { }
+  hero = 'Windstorm';
 
   ngOnInit(): void {
   }
